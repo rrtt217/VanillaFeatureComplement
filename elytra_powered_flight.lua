dd43080579742513f0fa25276f92fc983377b62e
rrtt217 <wwq169@mail.ustc.edu.cn>
2025-12-30 13:42:46 +0800

    Ensure only set speed when Player:IsElytraFlying()

--- elytra_powered_flight.lua
+++ elytra_powered_flight.lua
@@ -10,13 +10,14 @@
 function StartPoweredFilghtOnPlayerUsingItem(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX, CursorY, CursorZ)
     if Player:GetWorld():GetBlock(Vector3i(BlockX,BlockY,BlockZ)) == E_BLOCK_AIR and Player:GetEquippedItem().m_ItemType == E_ITEM_FIREWORK_ROCKET and Player:IsElytraFlying() then
         Player.ElytraFireWorkID = Player:GetWorld():CreateProjectile(Player:GetPosX(),Player:GetPosY(),Player:GetPosZ(),cProjectileEntity.pkFirework,Player,Player:GetEquippedItem(),Player:GetSpeed())
-        if Player:GetGameMode() == eGameMode.gmSurvival then
+        if Player:GetGameMode() == gmSurvival then
             Player:TossEquippedItem(1)
         end
         if Player.ElytraFireWorkID == 0 then
+            math.randomseed(os.time())
             -- Player.ElytraFireWorkTime = Player:GetEquippedItem().m_FireworkItem["FlightTimeInTicks"]
-            -- Can't get actual data, set to 20 ticks.
-            Player.ElytraFireWorkTime = 20
+            -- Can't get actual data, set to 20~60 ticks.
+            Player.ElytraFireWorkTime = math.random(20,60)
         end
     elseif Player:GetEquippedItem().m_ItemType == E_ITEM_FIREWORK_ROCKET and Player:IsElytraFlying() then
         return true
@@ -31,7 +32,7 @@ function SpeedUpPlayerOnTick(TimeDelta)
             Player:GetWorld():DoWithEntityByID(Player.ElytraFireWorkID or 0,
             ---@param Entity cEntity
             function (Entity)
-                if Lastpos then
+                if Lastpos and Player:IsElytraFlying() then
                     local look_vector = Vector3d(Player:GetLookVector())
                     -- LOG("Speed:".. "x" .. tostring(speed.x) .. "y"  .. tostring(speed.y) .. "z" .. tostring(speed.z))
                     -- Speed always returns 0, so use pos - lastpos.
@@ -42,7 +43,7 @@ function SpeedUpPlayerOnTick(TimeDelta)
                 end
             end
         )
-            if (not Player.ElytraFireWorkID or Player.ElytraFireWorkID == 0) and (Player.ElytraFireWorkTime and Player.ElytraFireWorkTime >= 0) and Lastpos then
+            if (not Player.ElytraFireWorkID or Player.ElytraFireWorkID == 0) and (Player.ElytraFireWorkTime and Player.ElytraFireWorkTime >= 0) and Lastpos and Player:IsElytraFlying() then
                 local look_vector = Vector3d(Player:GetLookVector())
                 -- LOG("Speed:".. "x" .. tostring(speed.x) .. "y"  .. tostring(speed.y) .. "z" .. tostring(speed.z))
                 local final_speed = Vector3d((0.85*(Player:GetPosX() - Lastpos.x) + 0.5*look_vector.x)/TimeDelta*1000 ,(0.85*(Player:GetPosY() - Lastpos.y) + 0.5*look_vector.y)/TimeDelta*1000 ,(0.85*(Player:GetPosZ() - Lastpos.z) + 0.5*look_vector.z)/TimeDelta*1000 )
